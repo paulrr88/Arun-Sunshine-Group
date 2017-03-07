59acfe8cbe59cdb254163a6e4b340308031aed43
paulrr88 <paul@Pauls-iMac.home>
2017-03-07 14:55:21 +0000

    Basic files added

---
+++ index.php
@@ -0,0 +1,351 @@
+
+
+<?php include('perch/runtime.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Home &mdash; The New Montessori Pre-school</title>
+  <meta name="description" content="The New Montessori Pre-school is run by parents for parents. It is the only accredited Montessori pre-school in Worthing and we are proud to be the only Montessori Charity pre-school in the UK. Our Management Committee ensures that the Montessori Method of education is offered to all children at an affordable price, with highly qualified and dedicated staff, and a high ratio of adults to children.">
+  <meta name="keywords" content="accredited, active, activity, "all day care", care, charity, childcare, children, classroom, daycare, development, educate, education, evening, flexible, infant, junior, kids, learning, lessons, local, maria, "Maria Montessori", method, montessori, nursery, pre-school, preschool, school, sussex, tarring, toddler, west, worthing, affordable, curriculum, early, eyfs, foundation, stage">
+  <meta name="author" content="The New Montessori Pre-school">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS & JavaScript
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css"><!-- reset file do not change -->
+  <link rel="stylesheet" href="css/skeleton.css"><!-- framework CSS -->
+  <link rel="stylesheet" href="css/style.css"><!--my additions-->
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+
+<body>
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <!--Container for ALL content, everything inside this--><div class="container">
+
+    <!--Row begins--><div class="row">
+
+      <!--Start of top information area--><div class="twelve columns" id="top">
+        <ul class="nostyle">
+          <li class="vertical-top"><a href="sitemap/">Sitemap</a></li>
+          <li><a href="<?php perch_content('Food rating link'); ?>"><img src="images/food.jpg" width="100" alt="Link to our food hygiene rating" /></a></li>
+          <li><img src="images/accredited.jpg" width="70" alt="This school is Montessori MEAB Accredited" /></a></li>
+
+        </ul>
+      </div><!--End of top information area-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+      <!--top link for bookmarks on long pages--><a name="top"></a>
+
+      <!--Header begin--><div class="twelve columns" id="header">
+
+        <div class="twelve columns">
+          <div id="header-logo"></div>
+
+          <div id="header-title-container">
+            <h1 id="header-title">
+              <span id="header-the">The</span>
+              <span id="header-new">New</span>
+              <span id="header-montessori">Montessori</span>
+              <span id="header-preschool">Pre-school</span>
+            </h1>
+          </div>
+        </div>
+
+      </div><!--Header ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div id="row">
+
+      <!--Navigation area begins--><div class="twelve columns" id="nav">
+        <!--Taglines at bottom of header-->
+
+        <?php include('header-contact.php')?>
+
+        <!--<div class="seven columns">
+          <p id="left">In the Anglican Diocese of Chichester, and Worthing Deanery</p>
+        </div>
+        <div class="five columns">
+          <p id="right">Church Lane, Ferring, West Sussex, BN12 5HN</p>
+        </div>-->
+        <div class="twelve columns" id="navbuttons">
+          <div id="navbuttons-aligner">
+            <a class="nav" href=""><div id="home">Home</div></a>
+            <a class="nav" href="about/"><div id="about">About Us</div></a>
+            <a class="nav" href="datesandfees/"><div id="dates">Dates &amp; Fees</div></a>
+            <a class="nav" href="mymontessorichild/"><div id="child">My Montessori Child</div></a>
+            <a class="nav" href="contact/"><div id="contact">Contact Us</div></a>
+            <a class="nav" href="https://www.facebook.com/The-New-Montessori-Pre-School-210642929391/"><div id="facebook">Facebook</div></a>
+          </div>
+        </div>
+      </div><!--Navigation area ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+      <!--Content begins #content--><div class="twelve columns nomargin" id="content">
+
+        <br />
+        <br />
+
+        <!--top home page area-->
+
+        <div class="six columns"><!--left column-->
+          <div class="twelve columns background-orange-light"><!--left column top-->
+            <div class="twelve columns">
+                <?php perch_content('Image 1'); ?>
+            </div>
+
+            <p class="centre">The New Montessori Pre-school is run by parents for parents. It is the only accredited Montessori pre-school in Worthing and we are proud to be the only Montessori Charity pre-school in the UK. Our <a href="about/team/#manage">Management Committee</a> ensures that the <a href="about/method/">Montessori Method of education</a> is offered to all children at an affordable price, with highly qualified and dedicated staff, and a high ratio of adults to children.</p>
+
+            <br />
+          </div>
+
+          <div class="spacer"></div>
+
+          <a href="about/" class="nostyle">
+            <div class="twelve columns background-orange-dark">
+              <h3 class="title linkbutton background-orange-dark">Learn more about us!</h3>
+            </div>
+          </a>
+
+          <div class="spacer"></div>
+
+
+          <div class="twelve columns background-orange-dark">
+            <p class="centre"><strong><a href="<?php perch_content('Link to MCI'); ?>" class="nostyle">Visit Montessori Centre International</a></strong></p>
+          </div>
+
+          <div class="spacer"></div>
+        </div>
+
+
+
+
+
+        <div class="five columns">
+          <div class="twelve columns background-blue">
+            <h2 class="mission">Our mission</h2>
+
+            <ul class="lesspadding">
+              <li>To allow the child to develop his/her unique personality</li>
+              <li>To help the child to become socially and emotionally well adjusted and to help him/her become physically strong</li>
+              <li>To make it possible for the child to develop to his/her intellectual capacity</li>
+            </ul>
+          </div>
+
+          <div class="twelve columns">
+            <br />
+
+            <p>We are a non-profit organisation providing Montessori education for pre-school children in the Worthing area. We are a registered charity (number 1086853), and you are welcome to view our <a href="<?php perch_content('Link to Charity Commission register entry'); ?>">Charity Commission register entry</a>.</p>
+
+            <p>We are also a registered company (number 3908382); details available from <a href="<?php perch_content('Link to Companies House entry'); ?>">Companies House</a>.</p>
+
+            <p><a href="about/"><strong>Learn more about us!</strong></a></p>
+
+            <br />
+          </div>
+
+          <div class="twelve columns background-orange-dark">
+            <h3 class="linkbutton"><?php perch_content('Link to EYFS document'); ?></h3>
+
+            <p class="centre">Our curriculum is based on this!</p>
+          </div>
+        </div>
+
+
+
+        <div class="spacer"></div>
+
+
+        <!--Elements of Diversity-->
+
+        <div id="diversity" class="twelve columns" style="">
+          <h2 class="pretty-purple">Elements of Diversity in a Montessori Classroom</h2>
+
+          <br />
+          <br />
+
+          <div>
+            <div class="one-third column" id="preparation">
+              <p><strong>Preparation</strong></p>
+
+              <p><strong>Ensuring the environment is laid out in the correct order. Leads to the environment being a successful part of the child's development. A happy friendly place where children feel at home.</strong></p>
+
+              <br />
+              <br />
+            </div>
+            <div class="one-third column" id="adultteacher">
+              <p><strong>Adult Teacher</strong></p>
+
+              <p><strong>"Directress/Director"</strong></p>
+
+              <p><strong>To observe the child develop and have a natural understanding of the child's requirements. To direct the natural energies of the child that they see emerging.</strong></p>
+
+              <br />
+            </div>
+            <div class="one-third column" id="absorbentmind">
+              <p><strong>Absorbent Mind</strong></p>
+
+              <p><strong>Children literally absorb the world around them, true discipline and harmony come from within the child and is not something that should be forced.</strong></p>
+
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          </div>
+
+          <div class="spacer"></div>
+
+          <div class="u-cf">
+            <div class="one-third column" id="lifeskills">
+              <p><strong>Life Skills</strong></p>
+
+              <p><strong>Acquired through child sized equipment, which inspires the child to become independent and in control of his life. Areas include motor control, care of the environment, care of oneself, grace and courtesy.</strong></p>
+            </div>
+            <div class="one-third column" id="childteacher">
+              <p><strong>Child Teacher</strong></p>
+
+              <p><strong>Natural way to learn is to watch and listen to older/younger children. When one child teaches another it confirms the child's knowledge. By working as a group the children create a community.</strong></p>
+
+              <br />
+              <br />
+            </div>
+            <div class="one-third column" id="childsize">
+              <p><strong>Child Size</strong></p>
+
+              <p><strong>All equipment and furniture is made to the size of the child. The classroom is the children's not the adult's. It needs to be clean, tidy, and complete to appeal to the child.</strong></p>
+            </div>
+          </div>
+
+          <div class="spacer"></div>
+
+          <div class="u-cf">
+            <div class="one-third column" id="learningsenses">
+              <p><strong>Learning Through Senses</strong></p>
+
+              <p><strong>Directing the child to educate himself through all the senses experiencing everything fully. The sensorial equipment in the classroom is designed to refine the child's senses.</strong></p>
+
+              <br />
+            </div>
+            <div class="one-third column" id="sensitive">
+              <p><strong>Sensitive Periods</strong></p>
+
+              <p><strong>If a child is left to follow these natural interests the child can achieve far more than would be normally expected. The Directress will observe the child to ensure the child is able to quench his thirst for knowledge.</strong></p>
+            </div>
+            <div class="one-third column" id="joylearning">
+              <p><strong>Joy of Learning</strong></p>
+
+              <p><strong>Each child works for themselves following their natural instincts. A child works to construct himself, an adult works to finish the job. The child receives great joy from finishing a task independently.</strong></p>
+            </div>
+          </div>
+
+          <div class="spacer"></div>
+
+          <div class="u-cf">
+            <div class="one-third column" id="equipment">
+              <p><strong>Montessori Equipment</strong></p>
+
+              <p><strong>Specifically designed equipment used to isolate information that moves the child from the unknown to the known through a sequence of events. Allowing the child to put order to his mind.</strong></p>
+            </div>
+            <div class="one-third column" id="loveorder">
+              <p><strong>Love of Order</strong></p>
+
+              <p><strong>To enable the child to place each object in relation to its environment. Provides the child with a freedom to move on intellectually and develop in the knowledge of a safe environment.</strong></p>
+            </div>
+            <div class="one-third column" id="imagination">
+              <p><strong>Imagination</strong></p>
+
+              <p><strong>Activities based on the real world. Child sized equipment enables the child to explore everyday items and re live them through role-play.</strong></p>
+
+              <br />
+              <br />
+            </div>
+          </div>
+          <br />
+        </div><!--Elements of Diversity ends-->
+
+
+
+
+
+
+
+
+
+
+
+      </div><!--Content ends-->
+
+
+
+    </div><!--Row ends-->
+
+    <div class="row">
+
+
+
+    </div>
+
+
+
+    <!--Row begins--><div class="row">
+
+      <!--Footer begins #footer--><div class="twelve columns" id="footer">
+
+        <br />
+
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="about/">What is Montessori?</a></li>
+          <li><a href="datesandfees/">Dates &amp; Fees</a></li>
+          <li><a href="contact/">Contact Us</a></li>
+          <li><a href="mymontessorichild/">My Montessori Child</a></li>
+          <li><a href="about/method/">The Montessori Method</a></li>
+          <li><a href="about/history/">History of Montessori and Our Pre-school</a></li>
+          <li><a href="about/team/">Our Team</a></li>
+          <li><a href="about/policies/">Our Policies &amp; Documents</a></li>
+          <li><a href="https://www.facebook.com/The-New-Montessori-Pre-School-210642929391/">Facebook</a></li>
+        </ul>
+
+        <p><strong>&copy; <?php echo date('Y'); ?> The New Montessori Pre-school | Designed by <a href="http://www.paulcomputerhelp.co.uk/">Paul Robinson</a></strong><br />
+        Registered charity no. 1086853<br />
+        Company Number 3908382<br />
+        <br />
+        <a href="perch/">Perch Login</a></p>
+        <br />
+        <br />
+        <br />
+
+      </div><!--Footer ends-->
+
+    </div><!--Row ends-->
+
+  </div><!--Container ends-->
+
+  
+<!-- End Document
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
